342f47bcb71823dd6b5d576b1f0f1da7641cac0e
Five52 <five52@laposte.net>
2016-11-29 23:43:08 +0100

    Add armor and weapon api

--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -9,62 +9,42 @@ router.get('/', function(req, res, next) {
 
 router.get('/insertSpells', function(req, res, next) {
     mongodb.insertSpells().then(() => {
-        res.render('index', {title: 'Données ajoutées'});
+        res.render('index', {title: 'Compétences ajoutées'});
     });
 });
 
-router.get('/classes', function(req, res, next) {
-    mongodb.getClasses().then((classes) => {
-        res.json(classes);
-    })
-})
+router.get('/insertArmors', function(req, res, next) {
+    mongodb.insertArmors().then(() => {
+        res.render('index', {title: 'Armures ajoutées'});
+    });
+});
+
+router.get('/insertWeapons', function(req, res, next) {
+    mongodb.insertWeapons().then(() => {
+        res.render('index', {title: 'Armes ajoutées'});
+    });
+});
 
 router.get('/test', function(req, res, next) {
     res.render('test');
 });
 
-router.get('/api/test', function(req, res, next) {
-    res.json(require('../data/spell.json'));
-});
-
-router.get('/base', function(req, res, next) {
-    res.json(require('../../flare.json'));
-});
-
-router.get('/passives', function(req, res, next) {
-    mongodb.getAllPassives().then((passives) => {
-        res.json(passives);
-    });
-});
-
-router.get('/passives/:className', function(req, res, next) {
-    mongodb.getClassPassives(req.params.className).then((passives) => {
-        res.json(passives);
-    });
-});
-
-router.get('/actives', function(req, res, next) {
-    mongodb.getAllActives().then((actives) => {
-        res.json(actives);
-    });
-});
-
-router.get('/actives/:className', function(req, res, next) {
-    mongodb.getClassActives(req.params.className).then((actives) => {
-        res.json(actives);
-    });
-});
-
-router.get('/actives/:className/:category', function(req, res, next) {
-    mongodb.getClassCategoryActives(req.params.className, req.params.category).then((categories) => {
-        res.json(categories);
-    });
-});
-
-router.get('/spells', function(req, res, next) {
+router.get('/api/spells', function(req, res, next) {
     mongodb.getSpells().then((json) => {
         res.json(json);
     });
-})
+});
+
+router.get('/api/armors', function(req, res, next) {
+    mongodb.getArmors().then((json) => {
+        res.json(json);
+    });
+});
+
+router.get('/api/weapons', function(req, res, next) {
+    mongodb.getWeapons().then((json) => {
+        res.json(json);
+    });
+});
 
 module.exports = router;
--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -18,6 +18,9 @@ const obj = {
             });
         });
     },
+
+    // Spells
+
     insertSpells() {
         return new Promise((resolve, reject) => {
             const collection = this.instance.collection('spells');
@@ -182,7 +185,203 @@ const obj = {
                 });
             });
         })
-    }
+    },
+
+    // Armors
+
+    insertArmors() {
+        return new Promise((resolve, reject) => {
+            const collection = this.instance.collection('armors');
+            const armors = require('../data/armor.json');
+            collection.insert(armors, (err, records) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    },
+    getArmorTypes() {
+        return new Promise((resolve, reject) => {
+            const collection = this.instance.collection('armors');
+            collection.distinct('type', ((err, types) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(types);
+            }));
+        });
+    },
+    getArmorQualities() {
+        return new Promise((resolve, reject) => {
+            const collection = this.instance.collection('armors');
+            collection.distinct('quality', ((err, qualities) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(qualities);
+            }));
+        });
+    },
+    getTypeQualityArmors(type, quality) {
+        return new Promise((resolve, reject) => {
+            const collection = this.instance.collection('armors');
+            collection
+                .find({type: type, quality: quality})
+                .toArray((err, armors) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(armors);
+                });
+        });
+    },
+    getTypeArmors(type) {
+        return new Promise((resolve, reject) => {
+            let json = [];
+            let total = 0;
+            this.getArmorQualities().then((qualities) => {
+                qualities.forEach((quality) => {
+                    total++;
+                    this.getTypeQualityArmors(type, quality).then((armors) => {
+                        if (armors.length !== 0) {
+                            json.push({
+                                'name': quality,
+                                'children': armors,
+                                'treeLevel': 3
+                            });
+                        }
+                        if (--total === 0) {
+                            resolve(json);
+                        }
+                    });
+                });
+            });
+        })
+    },
+    getArmors() {
+        return new Promise((resolve, reject) => {
+            let json = {
+                'name': 'types',
+                'children': [],
+                'treeLevel': 1
+            };
+            let total = 0;
+            this.getArmorTypes().then((types) => {
+                types.forEach((type) => {
+                total++;
+                    this.getTypeArmors(type).then((armors) => {
+                        json.children.push({
+                            'name' : type,
+                            'children' : armors,
+                            'treeLevel': 2
+                        });
+                        if (--total === 0) {
+                            resolve(json);
+                        }
+                    });
+                });
+            });
+        })
+    },
+
+    // Weapons
+
+    insertWeapons() {
+        return new Promise((resolve, reject) => {
+            const collection = this.instance.collection('weapons');
+            const weapons = require('../data/weapon.json');
+            collection.insert(weapons, (err, records) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    },
+    getWeaponTypes() {
+        return new Promise((resolve, reject) => {
+            const collection = this.instance.collection('weapons');
+            collection.distinct('type', ((err, types) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(types);
+            }));
+        });
+    },
+    getWeaponQualities() {
+        return new Promise((resolve, reject) => {
+            const collection = this.instance.collection('weapons');
+            collection.distinct('quality', ((err, qualities) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(qualities);
+            }));
+        });
+    },
+    getTypeQualityWeapons(type, quality) {
+        return new Promise((resolve, reject) => {
+            const collection = this.instance.collection('weapons');
+            collection
+                .find({type: type, quality: quality})
+                .toArray((err, weapons) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(weapons);
+                });
+        });
+    },
+    getTypeWeapons(type) {
+        return new Promise((resolve, reject) => {
+            let json = [];
+            let total = 0;
+            this.getWeaponQualities().then((qualities) => {
+                qualities.forEach((quality) => {
+                    total++;
+                    this.getTypeQualityWeapons(type, quality).then((weapons) => {
+                        if (weapons.length !== 0) {
+                            json.push({
+                                'name': quality,
+                                'children': weapons,
+                                'treeLevel': 3
+                            });
+                        }
+                        if (--total === 0) {
+                            resolve(json);
+                        }
+                    });
+                });
+            });
+        })
+    },
+    getWeapons() {
+        return new Promise((resolve, reject) => {
+            let json = {
+                'name': 'types',
+                'children': [],
+                'treeLevel': 1
+            };
+            let total = 0;
+            this.getWeaponTypes().then((types) => {
+                types.forEach((type) => {
+                total++;
+                    this.getTypeWeapons(type).then((weapons) => {
+                        json.children.push({
+                            'name' : type,
+                            'children' : weapons,
+                            'treeLevel': 2
+                        });
+                        if (--total === 0) {
+                            resolve(json);
+                        }
+                    });
+                });
+            });
+        })
+    },
 }
 
 module.exports = obj;
